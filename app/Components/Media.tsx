720e4eeeb76a4063f2026ed93afe2c43a598fe12
kartik <kartikvyas02@gmail.com>
2024-03-20 20:25:09 +0530

    commit 5

--- app/Components/Media.tsx
+++ app/Components/Media.tsx
@@ -7,14 +7,14 @@ import ReactPlayer from 'react-player';
 
 function Media() {
     const topTenAvatar = AiAvatarsData.slice(0, 10);
-    const [hoveredVideo, setHoveredVideo] = useState(null);
+    const [isHovered, setIsHovered] = useState(false);
 
-    const handleMouseEnter = (videoId) => {
-        setHoveredVideo(videoId);
+    const handleMouseEnter = () => {
+        setIsHovered(true);
     };
 
     const handleMouseLeave = () => {
-        setHoveredVideo(null);
+        setIsHovered(false);
     };
 
     return (
@@ -23,17 +23,17 @@ function Media() {
             <p className='text-lg font-bold mb-2 mt-2'>Stock Video</p>
             <div className="flex overflow-x-auto">
                 {StockVideo.map((video) => (
-                    <div key={video.id} className="video-container mx-4">
+                    <div key={video.id} className="video-container mx-4"
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
                         <Paper
                             className="video-player relative rounded-lg shadow-lg"
                             style={{ width: "142px", height: "80px" }}
                             key={video.id}
-                            onMouseEnter={() => handleMouseEnter(video.id)}
-                            onMouseLeave={handleMouseLeave}
                         >
                             <ReactPlayer
                                 url={video.video_link}
-                                playing={hoveredVideo === video.id}
+                                playing={isHovered}
                                 controls={false}
                                 loop={true}
                                 width="100%"
